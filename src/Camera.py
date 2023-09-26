91056da2e739da9863bc8d5a211efaa7729c3adb
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-09-26 14:34:34 +0200

    Show the camera in a window

---
+++ src/Camera.py
@@ -0,0 +1,51 @@
+import cv2
+
+
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 6:  # if there are more than 5 non working ports stop the testing.
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." % dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" % (dev_port, h, w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." % (dev_port, h, w))
+                available_ports.append(dev_port)
+        dev_port += 1
+    return available_ports, working_ports, non_working_ports
+
+list_ports()
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(2)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
+
+
+
