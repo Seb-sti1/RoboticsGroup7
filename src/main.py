f60d30fb3daead7d778e5190872457cdb7e31b93
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-09-27 19:34:15 +0200

    Add visualization and send command to the simulator

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import threading
+import time
+
 from Robot import Robot
-from src.utils import deg_to_rad
+from src.Visualizer import Visualizer
+from src.utils import deg_to_rad, rad_to_deg
 
 if __name__ == '__main__':
     bound_angle_rad = [(deg_to_rad(tup[0]), deg_to_rad(tup[1])) for tup in
@@ -9,9 +13,23 @@ if __name__ == '__main__':
                       bound_angle_rad,
                       [0.05, 0, 0, 0],
                       [0, 0.093, 0.093, 0.05],
-                      [3.14/2, 0, 0, 0])
+                      [3.14 / 2, 0, 0, 0],
+                      simulation=True)
+    v = Visualizer(our_robot)
 
-    our_robot.move_to_deg([10, -10, -10, -10])
+    def cmd():
+        our_robot.set_speed(0.1)
+        our_robot.move_to_deg([10, -10, -10, -10])
+        time.sleep(5)
+
+        our_robot.set_speed(0.5)
+        our_robot.move_to_deg([0, 90, 0, 0])
+        time.sleep(5)
+
+    cmd_thread = threading.Thread(target=cmd)
+    cmd_thread.start()
+
+    while cmd_thread.is_alive():
+        v.viz()
+
     our_robot.stop()
-
-
