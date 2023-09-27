fc2d0f65522b78cfef927e9b07055e84b2c7ac65
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-09-27 15:52:06 +0200

    Robot: minor fixes

--- src/Robot.py
+++ src/Robot.py
@@ -1,12 +1,22 @@
 import dynamixel_sdk.src.dynamixel_sdk as dxl
 from src.Servo import Servo
+from src.utils import rad_to_deg, deg_to_rad
 
 BAUDRATE = 1000000  # Baudrate for Motors
-DEVICENAME = '/dev/ttyACM0'  # Check which port is being used on your controller
-# ex) Windows: 'COM1'   Linux: '/dev/ttyUSB0' Mac: '/dev/tty.usbserial-*'
+DEVICENAME = '/dev/ttyACM0'  # the port of the controller examples: Windows: 'COM1' Linux: '/dev/ttyUSB0'
+
 
 class Robot:
-    def __init__(self, ids, angles, port=DEVICENAME):
+    def __init__(self, ids, bound_angles, d, a, alpha, port=DEVICENAME):
+        """
+        :param ids: the ids of the motors
+        :param bound_angles: an array of the bound angles of the motors
+        :param d: the d parameters of the Denavit Hartenberg table
+        :param a: the a parameters of the Denavit Hartenberg table
+        :param alpha: the alpha parameters of the Denavit Hartenberg table
+        :param port: the port of the controller
+        """
+        self.dh_parameters = [d, a, alpha]
 
         self.port = port
 
@@ -23,21 +33,30 @@ class Robot:
 
         # create the servos
         self.servos = []
-        for motor_id, angle in zip(ids, angles):
+        for motor_id, angle in zip(ids, bound_angles):
             self.servos.append(Servo(motor_id, angle[0], angle[1], self.port_handler))
-
-        # enable all servos
-        for servo in self.servos:
-            servo.enable_motor()
-            servo.set_position(0)
 
     def move_to(self, angles):
         """
         Move the robot to the given angles
-        :param angles: a array of angles
+        :param angles: an array of angles in radians
         """
         if len(angles) != len(self.servos):
             raise Exception("The number of angles does not match the number of servos")
 
         for servo, angle in zip(self.servos, angles):
             servo.set_position(angle)
+
+    def move_to_deg(self, angles):
+        """
+        Move the robot to the given angles
+        :param angles: an array of angles in degrees
+        """
+        self.move_to([deg_to_rad(angle) for angle in angles])
+
+    def stop(self):
+        for servo in self.servos:
+            servo.disable_motor()
+
+        if self.port_handler is not None:
+            self.port_handler.closePort()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from Robot import Robot
-
+from src.utils import deg_to_rad
 
 if __name__ == '__main__':
-    our_robot = Robot([1, 2, 3, 4], [(-130, 130), (-180, 0), (-100, 100), (-100, 100)])
+    bound_angle_rad = [(deg_to_rad(tup[0]), deg_to_rad(tup[1])) for tup in
+                       [(-130, 130), (-180, 0), (-100, 100), (-100, 100)]]
 
-    our_robot.move_to([10, -10, -10, -10])
+    our_robot = Robot([1, 2, 3, 4],
+                      bound_angle_rad,
+                      [0.05, 0, 0, 0],
+                      [0, 0.093, 0.093, 0.05],
+                      [3.14/2, 0, 0, 0])
+
+    our_robot.move_to_deg([10, -10, -10, -10])
+    our_robot.stop()
+
 
