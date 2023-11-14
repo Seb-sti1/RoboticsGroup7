e251e684a816014629f77cee3b46094d5863cbc7
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-11-14 15:41:26 +0100

    Camera.py: detect interior and exterior of the ring

--- src/Camera.py
+++ src/Camera.py
@@ -1,4 +1,8 @@
 import cv2
+import numpy as np
+import math as ma
+
+from src.HSVPicker import pick_hsv
 
 
 def list_ports():
@@ -27,25 +31,99 @@ def list_ports():
         dev_port += 1
     return available_ports, working_ports, non_working_ports
 
-list_ports()
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(2)
+class Camera:
+    def __init__(self, tip_to_camera_tf, port=0, show=True, ):
+        self.port = port
+        self.camera = cv2.VideoCapture(port)
+        self.show = show
+        self.tip_to_camera_frame = tip_to_camera_tf
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+        if show:
+            cv2.namedWindow("camera")
+            cv2.namedWindow("debug")
+            cv2.namedWindow("debug1")
+            cv2.namedWindow("debug2")
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+    def get_frame(self):
+        frame = None
 
-cv2.destroyWindow("preview")
-vc.release()
+        if self.camera.isOpened():  # try to get the first frame
+            rval, frame = self.camera.read()
+        else:
+            rval = False
+
+        return frame if rval else None
+
+    def get_ring_position(self, contour_area_lower_threshold=1500, contour_area_upper_threshold=60000):
+        frame = self.get_frame()
+        if frame is None:
+            return None
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, (14, 45, 140), (50, 130, 255))
+        frame_mask = cv2.bitwise_and(frame, frame, mask=mask)
+        ret, threshold = cv2.threshold(cv2.cvtColor(frame_mask, cv2.COLOR_BGR2GRAY), 125, 255, cv2.THRESH_BINARY)
+
+        contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        ordered_contour = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+        print(sorted([cv2.contourArea(c) for c in contours])[::-1][:5])
+
+        # find the
+        outside_ellipse = None
+        inside_ellipse = None
+        for i, c in enumerate(ordered_contour):
+            if len(c) < 5 \
+                    or cv2.contourArea(c) < contour_area_lower_threshold \
+                    or cv2.contourArea(c) > contour_area_upper_threshold:
+                continue
+            try:
+                ellipse = cv2.fitEllipse(c)
+                if i == 0:
+                    outside_ellipse = ellipse
+                if i == 1:
+                    inside_ellipse = ellipse
+            except AssertionError as e:
+                pass
+
+        if outside_ellipse is not None and inside_ellipse is not None:
+            cv2.ellipse(frame, outside_ellipse, (0, 255, 0), 2)
+            cv2.ellipse(frame, inside_ellipse, (255, 0, 0), 2)
+
+        if self.show:
+            cv2.imshow("camera", frame)
+            cv2.imshow("debug", frame_mask)
+            cv2.imshow("debug", threshold)
+            key = cv2.waitKey(5)
+            if key == 27:  # exit on ESC
+                return False
+
+        return [0, 0, 0]
+
+    def close(self):
+        self.camera.release()
+        if self.show:
+            cv2.destroyWindow("camera")
+            cv2.destroyWindow("debug")
+            cv2.destroyWindow("debug2")
+            cv2.destroyWindow("debug3")
 
 
+if __name__ == '__main__':
+    c = Camera(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
 
+    Theta = -30 / 180 * ma.pi
+    Dist = 0.25
+    T04 = np.array([[ma.cos(Theta), - ma.sin(Theta), 0, Dist * ma.cos(Theta)],
+                    [ma.sin(Theta), ma.cos(Theta), 0, Dist * ma.sin(Theta)],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
+
+    while True:
+        ring = c.get_ring_position()
+
+        if ring is False:
+            break
+
+    c.close()
